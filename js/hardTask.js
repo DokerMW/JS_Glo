cd6e08ca14bba2a52e723c83aa07f8fd591e9f49
DokerMW <abddoker@gmail.com>
2024-03-25 16:10:52 +0500

    add: complete lesson3

--- js/hardTask.js
+++ js/hardTask.js
@@ -1,5 +1,6 @@
-const num = 266219;
+// Сложное задание №1
 
+const num = 266219;
 const numMult =  num.toString().split('').reduce((acc, item) => acc = acc * item);
 
 console.log(numMult);
@@ -8,3 +9,74 @@ const degree = numMult ** 3;
 
 console.log(degree);
 console.log(+(degree.toString().substring(0 , 2)));
+
+// Сложное задание №2
+
+let lang = 'ru';
+const date = new Date();
+const numberDay = date.getDay();
+
+const weekRu = [
+	'Воскресенье',
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+];
+const weekEng = [
+	'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+//Вывод дня недели с помощью if else
+if (lang == 'ru'){
+	console.log(weekRu[numberDay]);
+} else if (lang = 'en'){
+	console.log(weekEng[numberDay]);
+}
+
+//Вывод дня недели с помощью switch
+switch (lang){
+	case 'ru':
+		console.log(weekRu[numberDay]);
+		break;
+	case 'en':
+		console.log(weekEng[numberDay]);
+		break;
+}
+
+//Вывод дня недели через множественный массив
+const weekArray = [
+	['ru', 
+		weekRu
+	],
+	['en', 
+		weekEng
+	]
+];
+
+const currentLang = weekArray.filter(function (e) {
+  return e.indexOf(!lang)
+});
+
+console.log(currentLang[0][1][numberDay]);
+
+// Сложное задание №3
+let namePerson = 'Сергей';
+
+namePerson == 'Артем' 
+	? console.log('Директор') 
+	: namePerson == 'Александр' 
+	? console.log('Преподаватель') 
+	: console.log('Студент');
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-const title = "JS_Glo";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 1500;
-const rollback = 15;
-const fullPrice = 5000;
-const adaptive = true;
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?').toLowerCase().split(/[\s,]+/);
+const screenPrice = +prompt('Сколько будет стоить данная работа?');
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+const servise1 = prompt('Какой дополнительный тип услуги нужен?');
+const servisePrice1 = +prompt('Сколько это будет стоить?');
+const servise2 = prompt('Какой дополнительный тип услуги нужен?');
+const servisePrice2 = +prompt('Сколько это будет стоить?');
+const fullPrice = screenPrice + servisePrice1 + servisePrice2;
+const rollback = fullPrice * (15 / 100);
+const servicePercentPrice = Math.ceil(fullPrice - rollback);
+
+switch (true){
+	case 0 <= fullPrice && fullPrice <= 15000:
+		console.log('Скидка не предусмотрена!');
+		break;
+	case 15000 < fullPrice && fullPrice < 30000:
+		console.log('Скидка 5%!');
+		break;
+	case fullPrice > 30000:
+		console.log('Скидка 10%!');
+		break;
+	case fullPrice < 0:
+		console.log('Что то пошло не так');
+		break;
+}
 
 console.log(typeof title);
 console.log(typeof fullPrice);
@@ -11,9 +31,10 @@ console.log(typeof adaptive);
 console.log(screens.length);
 console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
 console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
-console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100));
+console.log(screens);
+console.log(`Откат ${rollback}`);
+console.log(`Стоимость с вычетом отката ${servicePercentPrice}`);
 
-alert("Hello world!");
+alert('Hello world!');
 
-console.log("Hello console!");
+console.log('Hello console!');
