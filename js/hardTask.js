049e52d0fc3d060756abbaaaaaa53137f9a376f4
DokerMW <abddoker@gmail.com>
2024-04-04 14:29:46 +0500

    add: hardtask lesson09

--- js/hardTask.js
+++ js/hardTask.js
@@ -1,22 +1,45 @@
-//Сложное задание №7
+//Сложное задание №9
 
-const date = new Date();
-const numberDay = date.getDay();
+const fullDate = new Date();
+const numberDay = fullDate.getDay();
+const dateOfMonth = fullDate.getDate();
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const monts = [
+ 'Декабря',
+ 'Января',
+ 'Февраля',
+ 'Марта',
+ 'Апреля',
+ 'Мая',
+ 'Июня',
+ 'Июля',
+ 'Августа',
+ 'Сентября',
+ 'Октября',
+ 'Ноября'
+];
 
-week.forEach((item, index) => {
-	const div = document.createElement('div');
-	div.innerHTML = item;
-	document.body.append(div);
-
-	if (item == 'Суббота' || item == 'Воскресенье'){
-		div.style.fontStyle = "italic";
-		console.log(`%c${item}`, 'font-style: italic');
-	}else if(index == numberDay - 1 ){
-		div.style.fontWeight = "bold";
-		console.log(`%c${item}`, 'font-weight: bold');
-	} else {
-		console.log(item);
+const changeEnding = (digit, endingOne, endingTwo, endingThree) =>{
+	let lastDigit = digit % 10;
+	if(lastDigit == 1){
+		return endingOne;
+	} else if (digit == 11 || digit == 12 || digit == 13 || digit == 14) {
+		return endingThree;
+	} else if (lastDigit == 2 || lastDigit == 3 || lastDigit == 4){
+		return endingTwo;
+	} else{
+		return endingThree;
 	}
-});
+}
+
+const printFullDate = `Сегодня ${week[numberDay]}, ${dateOfMonth} ${monts[dateOfMonth]} ${fullDate.getFullYear()} года, ${fullDate.getHours()} ${changeEnding(fullDate.getHours(), 'час', 'часа', 'часов')} ${fullDate.getMinutes()} ${changeEnding(fullDate.getMinutes(), 'минута', 'минуты', 'минут')} ${fullDate.getSeconds()} ${changeEnding(fullDate.getSeconds(), 'секунда', 'секунды', 'секунд')}`;
+
+const printShortDate = `${fullDate.toISOString().split('T')[0]} - ${fullDate.toISOString().split('T')[1].slice(0, -5)}`;
+
+const div = document.createElement('div');
+const divTwo = document.createElement('div');
+	div.innerHTML = printFullDate;
+	divTwo.innerHTML = printShortDate;
+	document.body.append(div);
+	document.body.append(divTwo);
